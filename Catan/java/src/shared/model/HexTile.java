fa7523b47abe4c0aa93afa1c73fdb7975d146373
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-01-15 22:49:42 -0700

    HexTile Modification

--- Catan/java/src/shared/model/HexTile.java
+++ Catan/java/src/shared/model/HexTile.java
@@ -1,5 +1,19 @@
 package shared.model;
 
+import shared.definitions.HexType;
+
 public class HexTile {
 
+	private HexType type;
+	
+	//Constructor
+	public HexTile(HexType type) {
+		this.type = type;
+	}
+	
+	
+	//Getters and Setters
+	public HexType getType() {
+		return this.type;
+	}
 }
