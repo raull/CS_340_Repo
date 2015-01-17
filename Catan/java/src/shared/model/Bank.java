47d32a3dfea5f1cdd025e7a61ea8965ca5e4f048
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-01-17 14:24:59 -0700

    Added Bank, some additional methods for UserManager

---
+++ Catan/java/src/shared/model/Bank.java
@@ -0,0 +1,15 @@
+package shared.model;
+
+public class Bank {
+	private DevCardDeck devCardDeck;
+	private ResourceCardDeck resourceCardDeck;
+	
+	public Bank(){
+		devCardDeck = new devCardDeck(/*some parameter to include all cards*/);
+		resourceCardDeck = new resourceCardDeck(/*same thing*/);
+	}
+	
+	
+	
+
+}
--- Catan/java/src/shared/model/UserManager.java
+++ Catan/java/src/shared/model/UserManager.java
@@ -2,24 +2,56 @@ package shared.model;
 
 import java.util.ArrayList;
 
+
+/**
+ * UserManger class manages the users and can change the player list, tell whose turn it is, and cycle through turns
+ * 
+ * @author thyer
+ *
+ */
 public class UserManager {
 	private ArrayList <Player> players;
-	private Game game;
-	private int currentUserIndex;
+	private int currentPlayerIndex;
+	private Player currentPlayer;
 	
-	public UserManager(Game game){
-		this.game = game;
+	public UserManager(){
 	}
 	
+	/**
+	 * Calls canAddUser, if that returns true, adds the new user to the player list
+	 * @param newPlayer the player to be added
+	 */
 	public void addUser(Player newPlayer){
 		players.add(newPlayer);
+		//TODO: throw exception if player can't be added
 	}
 	
+	/**
+	 * Checks to see if the Player looks okay and if the list is sufficiently small. 
+	 * @param newPlayer
+	 * @return true if the player can be added, false otherwise
+	 */
 	public boolean canAddUser(Player newPlayer){
 		return true;
 	}
 	
+	/**
+	 * Changes the current player to the next one in the list (or the first player if the end is reached).
+	 * Updates the currentPlayer and currentPlayerIndex
+	 */
 	public void cycleTurn(){
+		
+	}
+	
+	public Player getCurrentPlayer(){
+		return new Player(null, null);
+	}
+	
+	/**
+	 * Removes a given player from the list (not sure if we'd need this)
+	 * @param toBeRemovedPlayer
+	 */
+	public void removePlayer(Player toBeRemovedPlayer){
 		
 	}
 
