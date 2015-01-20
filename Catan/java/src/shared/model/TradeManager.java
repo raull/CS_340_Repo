33b985830b004f3343b2eb4d314d52b63c635ac1
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-01-19 20:07:09 -0700

    Trade Manager and Trade Offer documentation

--- Catan/java/src/shared/model/TradeManager.java
+++ Catan/java/src/shared/model/TradeManager.java
@@ -1,5 +1,57 @@
 package shared.model;
 
-public class TradeManager {
+import shared.model.board.Port;
+import shared.model.exception.InvalidMoveException;
 
+/**
+ * This class manages the trades between user-user and user-port
+ * @author Raul Lopez Villalpando
+ *
+ */
+public class TradeManager {
+	
+	/**
+	 * Make an offer between users
+	 * @param user1 The user making the offer
+	 * @param user2 The user receiving the offer
+	 * @param offer The offer that the first user is making
+	 * @throws InvalidMoveException
+	 */
+	public void makeOffer(User user1, User user2, TradeOffer offer) throws InvalidMoveException {
+		
+	}
+	
+	/**
+	 * Returns either if the offer can be made (if it's valid)
+	 * @param user1 The user making the offer
+	 * @param user2 The user receiving the offer
+	 * @param offer The offer that the first user is making
+	 * @return Whether or not the offer is valid
+	 */
+	public boolean canMakeOffer(User user1, User user2, TradeOffer offer) {
+		return false;
+	}
+	
+	/**
+	 * Make an offer between port and user
+	 * @param user The user making the offer
+	 * @param port The port receiving the offer
+	 * @param offer The offer that the first user is making
+	 * @throws InvalidMoveException
+	 */
+	public void makeOffer(User user, Port port, TradeOffer offer) throws InvalidMoveException {
+		
+	}
+	
+	/**
+	 * Returns either if the offer can be made (if it's valid)
+	 * @param user The user making the offer
+	 * @param port The port receiving the offer
+	 * @param offer The offer that the first user is making
+	 * @return Whether or not the offer is valid
+	 */
+	public boolean canMakeOffer(User user, Port port, TradeOffer offer) {
+		return false;
+	}
+	
 }
--- Catan/java/src/shared/model/TradeOffer.java
+++ Catan/java/src/shared/model/TradeOffer.java
@@ -1,5 +1,52 @@
 package shared.model;
 
+import shared.definitions.ResourceType;
+
+/**
+ * This class represents a trade offer
+ * @author Raul Lopez
+ *
+ */
 public class TradeOffer {
 
+	private int rate;
+	private ResourceType buy;
+	private ResourceType sell;
+	
+	/**
+	 * Create a new TradeOfferClass
+	 * @param offerRate The rate in which the offer will be performed
+	 * @param sellResource The resource that will be given
+	 * @param buyResource The resource that will be received
+	 */
+	public TradeOffer(int offerRate, ResourceType sellResource, ResourceType buyResource) {
+		this.rate = offerRate;
+		this.sell = sellResource;
+		this.buy = buyResource;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	public ResourceType getBuy() {
+		return buy;
+	}
+
+	public void setBuy(ResourceType buy) {
+		this.buy = buy;
+	}
+
+	public ResourceType getSell() {
+		return sell;
+	}
+
+	public void setSell(ResourceType sell) {
+		this.sell = sell;
+	}
+	
 }
