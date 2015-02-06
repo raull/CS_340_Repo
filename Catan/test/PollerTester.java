e696380ef40513f0b72319ffd302a6049d22d564
Jacobtk <jacobtknudsen@gmail.com>
2015-02-06 14:56:16 -0700

    more poller tester

--- Catan/test/PollerTester.java
+++ Catan/test/PollerTester.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import client.poller.Poller;
+import shared.definitions.DevCardType;
+import shared.definitions.HexType;
+import shared.definitions.ResourceType;
 import shared.locations.HexLocation;
 import shared.model.Model;
 import shared.model.board.HexTile;
 import shared.model.board.Map;
+import shared.model.cards.Bank;
+import shared.model.cards.DevCardDeck;
+import shared.model.cards.ResourceCardDeck;
 import shared.model.facade.ModelFacade;
 import shared.model.game.ScoreKeeper;
 import shared.model.game.TurnManager;
@@ -54,10 +60,95 @@ public class PollerTester
 		Map map = facade.map();
 		TurnManager turnManager = facade.turnManager();
 		ScoreKeeper score = facade.score();
+		Bank bank = facade.bank();
 		
-		//verify deck contents
+		//verify dev deck contents
+		DevCardDeck devdeck = bank.getDevCardDeck();
+		assertEquals(devdeck.getCountByType(DevCardType.MONOPOLY),2);
+		assertEquals(devdeck.getCountByType(DevCardType.MONUMENT),5);
+		assertEquals(devdeck.getCountByType(DevCardType.ROAD_BUILD),2);
+		assertEquals(devdeck.getCountByType(DevCardType.SOLDIER),14);
+		assertEquals(devdeck.getCountByType(DevCardType.YEAR_OF_PLENTY),2);
+		
 		//verify hextile types and numbers
+		HexTile tile = map.getHexTileByLocation(new HexLocation(0,-2));
+		assertEquals(tile.getType(), HexType.WOOD);
+		assertEquals(tile.getNumber(), 11);
+		
+		tile = map.getHexTileByLocation(new HexLocation(1,-2));
+		assertEquals(tile.getType(), HexType.SHEEP);
+		assertEquals(tile.getNumber(), 10);
+
+		tile = map.getHexTileByLocation(new HexLocation(2,-2));
+		assertEquals(tile.getType(), HexType.BRICK);
+		assertEquals(tile.getNumber(), 5);
+		
+		tile = map.getHexTileByLocation(new HexLocation(-1,-1));
+		assertEquals(tile.getType(), HexType.WOOD);
+		assertEquals(tile.getNumber(), 3);
+
+		tile = map.getHexTileByLocation(new HexLocation(0, -1));
+		assertEquals(tile.getType(), HexType.SHEEP);
+		assertEquals(tile.getNumber(), 9);
+
+		tile = map.getHexTileByLocation(new HexLocation(1, -1));
+		assertEquals(tile.getType(), HexType.WOOD);
+		assertEquals(tile.getNumber(), 4);
+
+		tile = map.getHexTileByLocation(new HexLocation(2, -1));
+		assertEquals(tile.getType(), HexType.WHEAT);
+		assertEquals(tile.getNumber(), 6);
+
+		tile = map.getHexTileByLocation(new HexLocation(-2, 0));
+		assertEquals(tile.getType(), HexType.ORE);
+		assertEquals(tile.getNumber(), 3);
+		
+		tile = map.getHexTileByLocation(new HexLocation(-1, 0));
+		assertEquals(tile.getType(), HexType.WHEAT);
+		assertEquals(tile.getNumber(), 8);
+
+		tile = map.getHexTileByLocation(new HexLocation(0,0));
+		assertEquals(tile.getType(), HexType.ORE);
+		assertEquals(tile.getNumber(), 5);
+
+		tile = map.getHexTileByLocation(new HexLocation(1, 0));
+		assertEquals(tile.getType(), HexType.BRICK);
+		assertEquals(tile.getNumber(), 4);
+
+		tile = map.getHexTileByLocation(new HexLocation(2, 0));
+		assertEquals(tile.getType(), HexType.SHEEP);
+		assertEquals(tile.getNumber(), 10);
+
+		tile = map.getHexTileByLocation(new HexLocation(-2,1));
+		assertEquals(tile.getType(), HexType.WHEAT);
+		assertEquals(tile.getNumber(), 2);
+
+		tile = map.getHexTileByLocation(new HexLocation(-1,1));
+		assertEquals(tile.getType(), HexType.WOOD);
+		assertEquals(tile.getNumber(), 6);
+
+		tile = map.getHexTileByLocation(new HexLocation(1,-2));
+		assertEquals(tile.getType(), HexType.DESERT);
+
+		tile = map.getHexTileByLocation(new HexLocation(1,1));
+		assertEquals(tile.getType(), HexType.SHEEP);
+		assertEquals(tile.getNumber(), 12);
+
+		tile = map.getHexTileByLocation(new HexLocation(-2,2));
+		assertEquals(tile.getType(), HexType.ORE);
+		assertEquals(tile.getNumber(), 9);
+
+		tile = map.getHexTileByLocation(new HexLocation(-1,2));
+		assertEquals(tile.getType(), HexType.BRICK);
+		assertEquals(tile.getNumber(), 8);
+
+		tile = map.getHexTileByLocation(new HexLocation(0,2));
+		assertEquals(tile.getType(), HexType.WHEAT);
+		assertEquals(tile.getNumber(), 11);
+		
 		//verify no roads, settlements, or cities on map
+		
+		
 		//verify port locations, types, and ratios
 		
 		//verify robber on 0,1
@@ -89,11 +180,13 @@ public class PollerTester
 		assertTrue(player0.ports().size() == 0);
 		assertTrue(score.getScoreValue(1) == 0);
 		
-		//empty log
-		
-		//empty chat
-		
 		//bank 24 resources each
+		ResourceCardDeck deck = bank.getResourceDeck();
+		assertEquals(deck.getCountByType(ResourceType.BRICK), 24);
+		assertEquals(deck.getCountByType(ResourceType.ORE), 24);
+		assertEquals(deck.getCountByType(ResourceType.SHEEP), 24);
+		assertEquals(deck.getCountByType(ResourceType.WHEAT), 24);
+		assertEquals(deck.getCountByType(ResourceType.WOOD), 24);
 		
 		//firstround
 		assertTrue(turnManager.currentTurnPhase() == TurnPhase.FIRSTROUND);
@@ -111,6 +204,10 @@ public class PollerTester
 		//version 0
 		Model model = facade.model;
 		assertEquals(model.getVersion(), 0);
+		
+		//empty log and chat
+		assertEquals(model.getLog().lines.size(), 0);
+		assertEquals(model.getChat().lines.size(), 0);
 	}
 
 }
