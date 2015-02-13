5da5e70ee39ba4bb8a437403f43f818bb28a3731
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-02-12 18:40:04 -0700

    Fixed an issue where the client would not start

--- Catan/java/src/client/map/MapControllerState.java
+++ Catan/java/src/client/map/MapControllerState.java
@@ -1,10 +1,20 @@
 package client.map;
 
 import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 
 public abstract class MapControllerState {
 
 	
 	public abstract boolean canPlaceRoad(EdgeLocation edgeLoc);
+	
+	public abstract boolean canPlaceSettlement(VertexLocation vertLoc);
+	
+	public abstract boolean canPlaceCity(VertexLocation vertLoc);
+	
+	public abstract boolean canPlaceRobber(HexLocation hexLoc);
+	
+	
 	
 }
