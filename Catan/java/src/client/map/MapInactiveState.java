697fcf2665f185a2dfd0d413bc6bed577e9eb017
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-02-12 18:56:08 -0700

    Extending children methods of MapControllerStates

--- Catan/java/src/client/map/MapInactiveState.java
+++ Catan/java/src/client/map/MapInactiveState.java
@@ -1,12 +1,32 @@
 package client.map;
 
 import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 
 public class MapInactiveState extends MapControllerState{
 
 	@Override
 	public boolean canPlaceRoad(EdgeLocation edgeLoc) {
 		//If the user isn't active, they can't interact with the map
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceSettlement(VertexLocation vertLoc) {
+		//If the user isn't active, they can't interact with the map
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceCity(VertexLocation vertLoc) {
+		//If the user isn't active, they can't interact with the map
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceRobber(HexLocation hexLoc) {
+		//TODO: logic
 		return false;
 	}
 
--- Catan/java/src/client/map/MapSetUpState.java
+++ Catan/java/src/client/map/MapSetUpState.java
@@ -1,6 +1,8 @@
 package client.map;
 
 import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
 import shared.model.facade.ModelFacade;
 import shared.model.game.User;
 
@@ -12,6 +14,24 @@ public class MapSetUpState extends MapControllerState{
 		ModelFacade tempFacade = new ModelFacade();
 		User tempUser = new User();
 		return tempFacade.canPlaceRoadAtLoc(tempFacade.turnManager(), edgeLoc, tempUser);
+	}
+
+	@Override
+	public boolean canPlaceSettlement(VertexLocation vertLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceCity(VertexLocation vertLoc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceRobber(HexLocation hexLoc) {
+		//Robber can't be placed during the first and second rounds
+		return false;
 	}
 
 }
