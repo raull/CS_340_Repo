23133f773604d721b1a140109480bd2ca86b4ba2
Jessie Young <j.young184@gmail.com>
2015-02-18 11:32:58 -0700

    added edge direction to port

--- Catan/java/src/shared/model/game/User.java
+++ Catan/java/src/shared/model/game/User.java
@@ -103,6 +103,10 @@ public class User {
 		return this.name.getName();
 	}
 	
+	public CatanColor getCatanColor() {
+		return color;
+	}
+	
 	public Color getColor() {
 		return color.getJavaColor();
 	}
