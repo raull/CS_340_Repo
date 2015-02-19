b554467baef23740d0311c4b67613a325be31968
Trenton Adam Hyer <thyer@pepperoni.cs.byu.edu>
2015-02-19 15:56:47 -0700

    Adding features for AI adding

--- Catan/java/src/client/join/PlayerWaitingController.java
+++ Catan/java/src/client/join/PlayerWaitingController.java
@@ -5,11 +5,12 @@ import java.util.Observer;
 
 import com.google.gson.JsonElement;
 
+import shared.model.game.User;
 import shared.proxy.ProxyException;
 import shared.proxy.ServerProxy;
 import shared.proxy.game.AddAIRequest;
-
 import client.base.*;
+import client.data.PlayerInfo;
 import client.manager.ClientManager;
 
 
@@ -19,8 +20,8 @@ import client.manager.ClientManager;
 public class PlayerWaitingController extends Controller implements IPlayerWaitingController, Observer {
 
 	public PlayerWaitingController(IPlayerWaitingView view) {
-
 		super(view);
+		ClientManager.instance().getModelFacade().addObserver(this);
 	}
 
 	@Override
@@ -31,19 +32,21 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
 
 	@Override
 	public void start() {
-		
 		getView().showModal();
+		attemptClose(); //immediately closes the modal if there's nothing to do
 	}
 
 	@Override
 	public void addAI() {
+		System.out.println("Adding AI in PlayerWaitingController");
 		
 		try {
 			AddAIRequest req = new AddAIRequest("LARGEST_ARMY"); //only type of AI supported by current server
 			//add AI from proxy
-			ClientManager.instance().getServerProxy().addAI(req);
+			JsonElement model = ClientManager.instance().getServerProxy().addAI(req);
+			ClientManager.instance().getModelFacade().updateModel(model);
+			attemptClose();
 			
-			getView().closeModal();
 		} catch (ProxyException e) {
 			e.printStackTrace();
 		}
@@ -53,8 +56,39 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		getView().closeModal();
+		ClientManager cm = ClientManager.instance();
+		System.out.println("Updating in PlayerWaitingController");
+		
+		for(User u : cm.getModelFacade().turnManager().getUsers()){ //iterates through all players
+			System.out.println("Seeing if user " + u.getName() + " exists already");
+			PlayerInfo newPlayer = new PlayerInfo();
+			newPlayer.setColor(u.getCatanColor());
+			newPlayer.setId(u.getPlayerID());
+			newPlayer.setName(u.getName());
+			newPlayer.setPlayerIndex(u.getTurnIndex());
+			
+			for(PlayerInfo pi : cm.getCurrentGameInfo().getPlayers()){ //checks all players already known about
+				System.out.println("Comparing user " + u.getName() + " to player " + pi.getName());
+				if(pi.getName().equals(u.getName())){ //if that player is already known, we don't need to add them
+					System.out.println("They're the same! Don't add");
+					newPlayer = null;
+				}
+			}
+			if(newPlayer!=null){ //if the player wasn't found, add him/her to the game
+				System.out.println("Adding new player to gameinfo");
+				cm.getCurrentGameInfo().addPlayer(newPlayer);
+			}
+		}
+		attemptClose();
+	}
+	
+	private void attemptClose(){
+		if(ClientManager.instance().getCurrentGameInfo().getPlayers().size()==4 
+				&& this.getView().isModalShowing()){ //modal only closes if there are four players
+			getView().closeModal();
+			//TODO start the game
+			System.out.println("Here's where we should start the game");
+		}
 	}
 
 }
