33f057a177e3ca8bc8486efe841446fe7531b827
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-02-19 23:42:19 -0700

    Disable selecting another user's color

--- Catan/java/src/client/join/JoinGameController.java
+++ Catan/java/src/client/join/JoinGameController.java
@@ -157,6 +157,9 @@ public class JoinGameController extends Controller implements IJoinGameControlle
 	public void startJoinGame(GameInfo game) {
 		ClientManager.instance().setCurrentGameInfo(game); //sets the currentGameInfo, which we use to save the gameID
 		getSelectColorView().showModal();
+		for(PlayerInfo pi : game.getPlayers()){
+			getSelectColorView().setColorEnabled(pi.getColor(), false);
+		}
 	}
 
 	@Override
