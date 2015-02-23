fd7480aa567fd977f733c36b8b5d293747493b72
Jessie Young <j.young184@gmail.com>
2015-02-23 11:04:42 -0700

    fix null pointers in point controller

--- Catan/java/src/client/points/PointsController.java
+++ Catan/java/src/client/points/PointsController.java
@@ -58,16 +58,20 @@ public class PointsController extends Controller implements IPointsController, O
 		ClientManager cm = ClientManager.instance();
 		
 		//get information of what client user's turn index is
-		int currPlayerIndex = cm.getCurrentPlayerInfo().getPlayerIndex();
+//		int currPlayerIndex = cm.getCurrentPlayerInfo().getPlayerIndex();
+		int currPlayerID = cm.getCurrentPlayerInfo().getId();
 		
 		//get the user from the turn index
-		User user = cm.getModelFacade().turnManager().getUser(currPlayerIndex);
+//		User user = cm.getModelFacade().turnManager().getUser(currPlayerIndex);
+		//get user by id
+		User user = cm.getModelFacade().turnManager().getUser(currPlayerID);
 		
 		//set the user's number of points
 		getPointsView().setPoints(user.getVictoryPoints());
 		
 		if(hasWinner) {
 			int winnerIndex = cm.getModelFacade().getWinnerIndex();
+			int currPlayerIndex = user.getTurnIndex();
 			boolean isLocalPlayer = (winnerIndex == currPlayerIndex);
 			String winnerName = cm.getModelFacade().turnManager().getUserFromIndex(winnerIndex).getName();
 			getFinishedView().setWinner(winnerName, isLocalPlayer);
