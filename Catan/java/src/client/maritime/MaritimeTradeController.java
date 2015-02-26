5a017b29d7e1c3973ae7458bd3d3e183ea445145
Jessie Young <j.young184@gmail.com>
2015-02-25 18:27:17 -0700

    starting on maritime trade stuff

--- Catan/java/src/client/maritime/MaritimeTradeController.java
+++ Catan/java/src/client/maritime/MaritimeTradeController.java
@@ -4,8 +4,11 @@ import java.util.Observable;
 import java.util.Observer;
 
 import shared.definitions.*;
+import shared.proxy.ProxyException;
+import shared.proxy.moves.MaritimeTrade;
 import client.base.*;
 import client.manager.ClientManager;
+import client.misc.MessageView;
 
 
 /**
@@ -14,6 +17,10 @@ import client.manager.ClientManager;
 public class MaritimeTradeController extends Controller implements IMaritimeTradeController, Observer {
 
 	private IMaritimeTradeOverlay tradeOverlay;
+	private ResourceType outResource;
+	private ResourceType inResource;
+	
+	private ClientManager cm = ClientManager.instance();
 	
 	public MaritimeTradeController(IMaritimeTradeView tradeView, IMaritimeTradeOverlay tradeOverlay) {
 		
@@ -39,13 +46,33 @@ public class MaritimeTradeController extends Controller implements IMaritimeTrad
 	@Override
 	public void startTrade() {
 		
+		//show the overlay
 		getTradeOverlay().showModal();
 	}
 
 	@Override
 	public void makeTrade() {
+		
+		int playerIndex = cm.getCurrentPlayerInfo().getPlayerIndex();
+		int ratio = 4; //change after checking for ports
+		String outputResource = outResource.toString();
+		String inputResource = inResource.toString();
 
-		getTradeOverlay().closeModal();
+		try {
+			
+			MaritimeTrade tradeMaritimeReq = new MaritimeTrade(playerIndex, ratio, outputResource, inputResource);
+			//get proxy to maritime trade
+			cm.getServerProxy().maritimeTrade(tradeMaritimeReq);
+			//close overlay after trade
+			getTradeOverlay().closeModal();
+		} catch (ProxyException e) {
+			e.printStackTrace();
+			MessageView alertView = new MessageView();
+			alertView.setTitle("Error");
+			alertView.setMessage(e.getLocalizedMessage());
+			alertView.showModal();
+		}
+		
 	}
 
 	@Override
@@ -56,12 +83,14 @@ public class MaritimeTradeController extends Controller implements IMaritimeTrad
 
 	@Override
 	public void setGetResource(ResourceType resource) {
-
+		//sets the resource user is getting
+		inResource = resource;
 	}
 
 	@Override
 	public void setGiveResource(ResourceType resource) {
-
+		//sets the resource user is giving
+		outResource = resource;
 	}
 
 	@Override
