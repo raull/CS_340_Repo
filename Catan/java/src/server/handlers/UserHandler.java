849211e7e446a2a501ed8c63ae8373ace0a781b8
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-03-11 21:02:20 -0600

    Created handlers, stubbed them out

---
+++ Catan/java/src/server/handlers/UserHandler.java
@@ -0,0 +1,18 @@
+package server.handlers;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class UserHandler implements HttpHandler{
+	public UserHandler(){
+
+	}
+	@Override
+	public void handle(HttpExchange arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
