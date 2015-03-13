62a5003835d9815eef937da6cbe4b23b77e6015d
Trenton Adam Hyer <thyer@bacon.cs.byu.edu>
2015-03-13 15:10:41 -0600

    Added javadoc to factory classes

--- Catan/java/src/server/handlers/factories/GamesCommandFactory.java
+++ Catan/java/src/server/handlers/factories/GamesCommandFactory.java
@@ -4,8 +4,18 @@ import client.base.IAction;
 
 import com.sun.net.httpserver.HttpExchange;
 
+/**
+ * A factory that delivers command objects for the GamesHandler. 
+ * @author thyer
+ *
+ */
 public class GamesCommandFactory {
 
+	/**
+	 * Creates and returns an IAction based on the instructions found within the HttpExchange object
+	 * @param arg0 the HttpExchange object
+	 * @return an IAction containing the appropriate behavior to be executed when appropriate
+	 */
 	public static IAction create(HttpExchange arg0) {
 		// TODO Auto-generated method stub
 		return null;
