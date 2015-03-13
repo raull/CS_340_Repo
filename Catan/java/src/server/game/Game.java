507966ff4e132d14fc04b66284788c54efddd4bb
Jessie Young <j.young184@gmail.com>
2015-03-13 12:15:32 -0600

    updating game/game manager

--- Catan/java/src/server/game/Game.java
+++ Catan/java/src/server/game/Game.java
@@ -1,5 +1,42 @@
 package server.game;
 
-public class Game {
+import shared.model.facade.ModelFacade;
 
+public class Game {
+	private int id; //the game's id
+	private String name; //game's name
+	private ModelFacade modelFacade;
+	
+	public Game(int id, String name, ModelFacade modelFacade) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.modelFacade = modelFacade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ModelFacade getModelFacade() {
+		return modelFacade;
+	}
+
+	public void setModelFacade(ModelFacade modelFacade) {
+		this.modelFacade = modelFacade;
+	}
+	
+	
 }
--- Catan/java/src/server/game/GameManager.java
+++ Catan/java/src/server/game/GameManager.java
@@ -1,5 +1,37 @@
 package server.game;
 
-public class GameManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameManager {
+	private List<Game> games;
+	
+	public GameManager() {
+		games = new ArrayList<Game>();
+	}
+	
+	/**
+	 * adds a new game to the existing list
+	 * @param newGame
+	 */
+	public void addGame(Game newGame) {
+		games.add(newGame);
+	}
+	
+	/**
+	 * get a game by its id
+	 * @param id
+	 * @return
+	 */
+	public Game getGameById(int id) {
+		return null;
+	}
+	
+	/**
+	 * get the list of existing games
+	 * @return
+	 */
+	public List<Game> getGames() {
+		return games;
+	}
 }
