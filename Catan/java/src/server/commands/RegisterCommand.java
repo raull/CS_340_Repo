5a5d6415ab11f72fb39ca676ccd625ee25569f3c
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-03-14 20:09:30 -0600

    Another example implementation

---
+++ Catan/java/src/server/commands/RegisterCommand.java
@@ -0,0 +1,17 @@
+package server.commands;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class RegisterCommand extends ServerCommand {
+
+	public RegisterCommand(HttpExchange arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(){
+		
+	}
+
+}
