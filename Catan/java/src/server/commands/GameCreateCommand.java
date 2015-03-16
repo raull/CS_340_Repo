76b722fa5cc890846cd935e87474fcdd32f13003
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-03-15 18:00:15 -0600

    Created GameCreate Command and made some modifications to the parent

---
+++ Catan/java/src/server/commands/GameCreateCommand.java
@@ -0,0 +1,17 @@
+package server.commands;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class GameCreateCommand extends ServerCommand {
+
+	public GameCreateCommand(HttpExchange arg0) {
+		super(arg0);
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Catan/java/src/server/commands/ServerCommand.java
+++ Catan/java/src/server/commands/ServerCommand.java
@@ -2,16 +2,14 @@ package server.commands;
 
 import com.sun.net.httpserver.HttpExchange;
 
-import client.base.IAction;
-
-public class ServerCommand{
-	HttpExchange httpObj;
+public abstract class ServerCommand{
+	
+	protected HttpExchange httpObj;
+	
 	public ServerCommand(HttpExchange arg0){
 		httpObj = arg0;
 	}
 
-	public void execute(){
-		
-	}
+	public abstract void execute();
 
 }
