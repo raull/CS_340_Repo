48e3e603e94713205e858fffba65ece39484572b
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-03-17 19:44:29 -0600

    Initial work on MockCommands

--- Catan/java/src/server/handler/Handler.java
+++ Catan/java/src/server/handler/Handler.java
@@ -1,6 +1,7 @@
 package server.handler;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 import server.command.ServerCommand;
 import server.exception.ServerInvalidRequestException;
@@ -12,10 +13,13 @@ import client.base.IAction;
 import com.google.gson.JsonElement;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class Handler implements HttpHandler{
 	
 	CommandFactory factory;
+	private XStream xmlStream = new XStream(new DomDriver());
 	
 	public Handler(boolean testing){
 		if(testing)
@@ -29,6 +33,9 @@ public class Handler implements HttpHandler{
 		ServerCommand event = factory.create(arg0);
 		try{
 			JsonElement response = event.execute();
+			arg0.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+			xmlStream.toXML(response, arg0.getResponseBody());
+			arg0.getResponseBody().close();
 		} catch(ServerInvalidRequestException e1){
 			//do stuff
 		}
