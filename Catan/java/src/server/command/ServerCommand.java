99ef94efd3710ca3249606ae5cda81d66025f1bd
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-03-17 19:45:50 -0600

    Started parsing cookie

--- Catan/java/src/server/command/ServerCommand.java
+++ Catan/java/src/server/command/ServerCommand.java
@@ -1,5 +1,8 @@
 package server.command;
 
+import java.util.List;
+
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 
 /**
@@ -10,14 +13,35 @@ import com.sun.net.httpserver.HttpExchange;
 public abstract class ServerCommand{
 	
 	protected HttpExchange httpObj;
+	protected int gameId;
+	protected int playerIndex;
 	
 	public ServerCommand(HttpExchange arg0){
 		httpObj = arg0;
+		httpObj.getRequestMethod();
+		
+		Headers headers = httpObj.getRequestHeaders();
+		List<String> cookies = headers.get("Cookie");
+		String catanCookie = cookies.get(cookies.size());
+		parseCookie(catanCookie);
 	}
 
 	/**
 	 * Action to execute. Override this method
 	 */
 	public abstract void execute();
+	
+	
+	private void parseCookie(String cookie) {
+		String[] parameters = cookie.split(";");
+		
+		for (String string : parameters) {
+			if (string.contains("catan.user")) {
+				
+			} else if (string.contains("catan.game")) {
+				
+			}
+		}
+	}
 
 }
