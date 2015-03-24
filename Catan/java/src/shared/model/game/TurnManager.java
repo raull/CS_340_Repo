f69222e9f3f6e34f35a9b10f23597ea80379e18e
Jessie Young <j.young184@gmail.com>
2015-03-24 13:38:36 -0600

    some changes with where clone user is called

--- Catan/java/src/shared/model/game/TurnManager.java
+++ Catan/java/src/shared/model/game/TurnManager.java
@@ -101,9 +101,10 @@ public class TurnManager {
 		int index = users.size();
 		
 		if(users.size() < 4) {
-			User userToAdd = newUser.clone();
-			newUser.setPlayerTurnIndex(index);
-			users.add(userToAdd);
+//			User userToAdd = newUser.clone();
+//			newUser.setPlayerTurnIndex(index);
+//			users.add(userToAdd);
+			users.add(newUser);
 		}
 		else{
 			throw new ModelException();
