1997ab2a8675c23b2619cf620350d0ad27c312ae
jy19 <jyoung19@football.cs.byu.edu>
2015-03-30 17:14:14 -0600

    starting on some stuff

--- Catan/test/server/ServerFacadeTester.java
+++ Catan/test/server/ServerFacadeTester.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
-import com.sun.net.httpserver.HttpExchange;
-
 import server.command.ServerCommand;
 import server.command.game.*;
 import server.exception.ServerInvalidRequestException;
@@ -58,25 +56,145 @@ public class ServerFacadeTester {
 		//color already chosen
 	}
 	
+	@Test
+	public void rollNumber() {
+		//incorrect turn phase
+		try {
+			facade.rollNumber(0, 0, 6);
+			//failed, no error caught
+			
+		} catch (ServerInvalidRequestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//not user's turn
+		try {
+			facade.rollNumber(0, 0, 6);
+			//failed, no error caught
+		} catch (ServerInvalidRequestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//ok test case, turnphase updated, players with buildings on number hex gain resources
+		try {
+			facade.rollNumber(0, 0, 6);
+			//assert equals that new model is as expected
+		} catch (ServerInvalidRequestException e) {
+			//fail, shouldn't have an exception
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void robPlayer() {
+		//incorrect turn phase
+		//not user's turn
+		//robber is in same place
+		//player being robbed has no resource cards
+		//ok test case - robber is moved, player robbed has given up resource
+	}
+	
+	@Test
+	public void finishTurn() {
+		//incorrect turn phase
+		//not user's turn
+		//ok test case, player index is updated
+	}
+	
+	@Test
+	public void buyDevCard() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have resources
+		//bank out of dev cards
+		//ok test case, user gains a dev card in newDevCards and loses resources
+	}
+	
+	@Test
+	public void yearOfPlenty() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have that card
+		//bank doesn't have wanted resources
+		//ok test case, user uses card and gets two cards from bank
+	}
+	
+	@Test
+	public void roadBuild() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have that card
+		//ok test case, user uses the card and builds two roads
+	}
+	
+	@Test
+	public void soldier() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have that card
+		//ok test case, user moves the robber and robs someone, if available
+	}
+	
+	@Test
+	public void monopoly() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have that card
+		//ok test case, user gains all resources from other players of resource type
+	}
+	
+	@Test
+	public void monument() {
+		//incorrect turn phase
+		//not user's turn
+		//user doesn't have that card
+		//ok test case, user gains a point and uses the card
+	}
+	
+	@Test
+	public void buildRoad() {
+		//incorrect turn phase
+		//not user's turn
+		//not enough resources for road
+		//not linked to another settlement or road owned by same user
+		//ok test case, user now has road
+	}
 	
 	@Test
 	public void buildSettlement() {
-		
+		//incorrect turn phase
+		//not user's turn
+		//not enough resources for settlement
+		//not linked to roads 
+		//invalid build location (next to some other player's settlement)
+		//ok test case, user now has settlement
 	}
 	
 	@Test
 	public void buildCity() {
-		
+		//incorrect turn phase
+		//not user's turn
+		//doesn't have enough resources for a city
+		//doesn't have a settlement in the place of a city
+		//ok test case, user now has a city
 	}
 	
 	@Test
 	public void offerTrade() {
+		//incorrect turn phase
+		//not user's turn
+		//don't have resources offering
+		//ok test case, model now has trade offer
 		
 	}
 	
 	@Test
 	public void acceptTrade() {
-		
+		//user not offered a trade
+		//if there is no trade offer
+		//user doesn't have required resources to accept
+		//user accepts the trade, resources for both players should be updated, trade offer removed
 	}
 	
 	@Test
@@ -85,11 +203,11 @@ public class ServerFacadeTester {
 		//not user's turn
 		//no necessary resources
 		//bank doesn't have card wanted
-		//regular maritime trade
+		//regular maritime trade, trade goes through, resources updated
 		//user gives 3, but doesn't have 3 port
 		//user gives 2, but doesn't have 2 port
-		//user has 3 port
-		//user has 2 port
+		//user has 3 port, trade goes through, resources updated
+		//user has 2 port, trade goes through, resources updated
 	}
 	
 	@Test
@@ -98,6 +216,6 @@ public class ServerFacadeTester {
 		//user doesn't have more than 7 cards
 		//user has more than 7 cards
 			//trying to discard cards they don't have
-			//ok test case
+			//ok test case, user loses their discarded cards
 	}
 }
