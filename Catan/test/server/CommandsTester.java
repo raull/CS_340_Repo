58d07bb25034ca1cb14c88516a45b0f8de48ef6e
Kent Joseph Pimentel <kpimente@hockey.cs.byu.edu>
2015-03-30 17:13:37 -0600

    setGame function added

---
+++ Catan/test/server/CommandsTester.java
@@ -0,0 +1,50 @@
+package server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.facade.ServerFacade;
+import server.handler.Handler;
+import shared.proxy.ProxyException;
+import shared.proxy.ServerProxy;
+import shared.proxy.games.JoinGameRequest;
+import shared.proxy.user.Credentials;
+
+public class CommandsTester {
+	private Handler handler;
+	
+	public CommandsTester(){
+		handler = new Handler(false);
+	}
+	private String host = "localhost";
+	private String port = "8081";
+	private ServerProxy testProxy = new ServerProxy(host, port);
+	
+	@Before
+	public void setUp() throws ProxyException{
+		testProxy.login(new Credentials("Sam", "sam"));
+		testProxy.join(new JoinGameRequest(0, "red"));
+		testProxy.login(new Credentials("Brooke", "brooke"));
+		testProxy.join(new JoinGameRequest(0, "orange"));
+		testProxy.login(new Credentials("Pete", "pete"));
+		testProxy.join(new JoinGameRequest(0, "blue"));
+		testProxy.login(new Credentials("Mark", "mark"));
+		testProxy.join(new JoinGameRequest(0, "green"));
+	}
+	
+	@Test
+	public void badRequests(){
+		
+	}
+	
+	@Test
+	public void malformedJSON(){
+		
+	}
+	
+	@Test
+	public void validRequests(){
+		
+	}
+
+}
