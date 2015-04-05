c0a33d1d933a9f843b455549ad25cf6c7168f83d
Trenton Adam Hyer <hyer.trent@gmail.com>
2015-04-05 13:10:10 -0600

    Created new packages, started DAOInterface stuff

---
+++ Catan/java/src/server/persistence/dao/DAOInterface.java
@@ -0,0 +1,16 @@
+package server.persistence.dao;
+
+public abstract class DAOInterface {
+	
+	public void startTransaction(){
+		//stuff happens
+	}
+	
+	public void endTranscation(boolean commit){
+		
+	}
+	
+	public void transact(String transaction){
+		
+	}
+}
