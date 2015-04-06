3d821295cd9dc90ebfe0b88e52c0c4c06bdec302
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-04-06 16:42:16 -0600

    Added unimplemented methods

--- Catan/java/src/server/persistence/dao/RelationalUserDAO.java
+++ Catan/java/src/server/persistence/dao/RelationalUserDAO.java
@@ -1,5 +1,9 @@
 package server.persistence.dao;
 
+import java.util.List;
+
+import shared.model.game.User;
+
 public class RelationalUserDAO extends UserDAOInterface{
 
 	@Override
@@ -9,15 +13,27 @@ public class RelationalUserDAO extends UserDAOInterface{
 	}
 
 	@Override
-	public boolean authenticateUser(String username, String password) {
+	public boolean register(String username, String password) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean register(String username, String password) {
+	public boolean saveUser(User user) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public User getUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> listUsers() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
