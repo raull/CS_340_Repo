a83ce810ce31da183fe5da08205003ae1b0d21ae
Trenton Adam Hyer <thyer@oven.cs.byu.edu>
2015-04-06 16:55:45 -0600

    Added new methods to Provider

--- Catan/java/src/server/persistence/provider/MockProvider.java
+++ Catan/java/src/server/persistence/provider/MockProvider.java
@@ -1,5 +1,11 @@
 package server.persistence.provider;
 
+import java.util.List;
+
+import server.game.Game;
+import server.persistence.provider.exception.DatabaseException;
+import shared.model.game.User;
+
 /**
  * Mock Provider for testing purposes
  * @author raulvillalpando
@@ -14,15 +20,33 @@ public class MockProvider implements Provider {
 	}
 
 	@Override
-	public void transact(String transaction) {
+	public void endTransaction(boolean commit) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void endTransaction(boolean commit) {
+	public List<Game> loadGames() {
 		// TODO Auto-generated method stub
-		
+		return null;
+	}
+
+	@Override
+	public boolean saveGames(List<Game> games) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<User> loadUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveUsers(List<User> users) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- Catan/java/src/server/persistence/provider/ProviderNonRelational.java
+++ Catan/java/src/server/persistence/provider/ProviderNonRelational.java
@@ -1,7 +1,12 @@
 package server.persistence.provider;
 
+import java.util.List;
+
+import server.game.Game;
 import server.persistence.factory.DAOFactory;
 import server.persistence.factory.NonrelationalDAOFactory;
+import server.persistence.provider.exception.DatabaseException;
+import shared.model.game.User;
 
 
 /**
@@ -28,9 +33,24 @@ public class ProviderNonRelational implements Provider {
 	}
 
 	@Override
-	public void transact(String transaction) {
+	public List<Game> loadGames() {
 		// TODO Auto-generated method stub
-		
+		return null;
+	}
+	@Override
+	public boolean saveGames(List<Game> games) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<User> loadUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean saveUsers(List<User> users) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
