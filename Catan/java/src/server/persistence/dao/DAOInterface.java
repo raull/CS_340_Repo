c1463e60769a30d88ab5d35be7f8a48d3c88b8d4
Trenton Adam Hyer <thyer@oven.cs.byu.edu>
2015-04-06 16:05:20 -0600

    Added firebomb method to DAOInterface

--- Catan/java/src/server/persistence/dao/DAOInterface.java
+++ Catan/java/src/server/persistence/dao/DAOInterface.java
@@ -2,4 +2,11 @@ package server.persistence.dao;
 
 public abstract class DAOInterface {
 	
+	/**
+	 * Resets the contents of the database to some initial state. Destroys all other table contents. 
+	 */
+	public void firebomb(){
+		
+	}
+	
 }
