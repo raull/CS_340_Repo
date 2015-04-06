6eabd0c6edef18d9e25e3a71d891b3b4794a3187
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-04-06 16:20:29 -0600

    UserDAO javadoc

--- Catan/java/src/server/persistence/dao/UserDAOInterface.java
+++ Catan/java/src/server/persistence/dao/UserDAOInterface.java
@@ -1,5 +1,26 @@
 package server.persistence.dao;
 
+/**
+ * Parent class to determine what a DAO for a User needs to implement.
+ * @author raulvillalpando
+ *
+ */
 public abstract class UserDAOInterface implements DAOInterface {
 
+	/**
+	 * Authenticate the user given the username and password
+	 * @param username The username of the user
+	 * @param password The password of the user
+	 * @return True if the user was authenticated successfully, False otherwise.
+	 */
+	abstract public boolean authenticateUser(String username, String password);
+	
+	/**
+	 * Register a user if the user doesn't exist already
+	 * @param username The username of the new user.
+	 * @param password The password of the new user.
+	 * @return True if the user was created and registered correctly, False otherwise.
+	 */
+	abstract public boolean register(String username, String password);
+	
 }
