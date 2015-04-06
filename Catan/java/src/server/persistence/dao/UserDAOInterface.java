5826683b9adc6a158695bd84a754babdcc80f72b
Raul Lopez Villalpando <ruffles91@gmail.com>
2015-04-06 16:39:05 -0600

    Updated UserDAO

--- Catan/java/src/server/persistence/dao/UserDAOInterface.java
+++ Catan/java/src/server/persistence/dao/UserDAOInterface.java
@@ -1,5 +1,9 @@
 package server.persistence.dao;
 
+import java.util.List;
+
+import shared.model.game.User;
+
 /**
  * Parent class to determine what a DAO for a User needs to implement.
  * @author raulvillalpando
@@ -8,12 +12,24 @@ package server.persistence.dao;
 public abstract class UserDAOInterface implements DAOInterface {
 
 	/**
-	 * Authenticate the user given the username and password
-	 * @param username The username of the user
-	 * @param password The password of the user
-	 * @return True if the user was authenticated successfully, False otherwise.
+	 * Update or save the state of a user.
+	 * @param user User that contains the ID and everything to save or update
+	 * @return True if the User got updated successfully, False otherwise.
 	 */
-	abstract public boolean authenticateUser(String username, String password);
+	abstract public boolean saveUser(User user);
+	
+	/**
+	 * Get an existing user by ID
+	 * @param id The ID of the existing user.
+	 * @return The user specified by the ID, null if it doesn't exist.
+	 */
+	abstract public User getUser(int id);
+	
+	/**
+	 * List all the registered users.
+	 * @return A list of all the existing users.
+	 */
+	abstract public List<User> listUsers();
 	
 	/**
 	 * Register a user if the user doesn't exist already
