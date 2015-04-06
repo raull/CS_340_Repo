a4d8432e89f150df3f0d8ba1b1d52693b89292b7
Trenton Adam Hyer <thyer@oven.cs.byu.edu>
2015-04-06 16:39:41 -0600

    Added all concrete DAOs

---
+++ Catan/java/src/server/persistence/dao/RelationalGameDAO.java
@@ -0,0 +1,40 @@
+package server.persistence.dao;
+
+import java.util.List;
+
+import server.game.Game;
+
+public class RelationalGameDAO extends GameDAOInterface{
+
+	@Override
+	public void firebomb() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean createGame(String name, boolean randomTiles,
+			boolean randomPorts, boolean randomNumbers) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Game getGame(int gameId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Game> listGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveGame(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Catan/java/src/server/persistence/dao/RelationalUserDAO.java
@@ -0,0 +1,23 @@
+package server.persistence.dao;
+
+public class RelationalUserDAO extends UserDAOInterface{
+
+	@Override
+	public void firebomb() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean authenticateUser(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean register(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
